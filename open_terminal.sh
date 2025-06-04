69a4ffa8e6c6f28e18b30c2221ba80c0799aee2d
ephremov <ephremovbox@gmail.com>
2025-06-04 16:14:11 +0700

    изменил имя пользователся в пути на нормальное

--- open_terminal.sh
+++ open_terminal.sh
@@ -3,7 +3,7 @@
 # Способ 1: Через команду open тест
 #open -a Terminal
 # Способ 2: Через команду open с указанием пути
- open -a Terminal /Users/username/Documents
+ open -a Terminal /Users/ephremov/Documents
 # Способ 3: Через команду open с указанием пути и параметром -n для нового окна
 # open -n -a Terminal /Users/username/Documents
 # Способ 4: Через команду open с указанием пути и параметром -a для приложения
