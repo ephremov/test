a2ce93eebd3774bb25be6a3394288fbf641d4776
ephremov <ephremovbox@gmail.com>
2025-06-04 16:11:46 +0700

    изменил запуск терминала с указанием пути

--- open_terminal.sh
+++ open_terminal.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Способ 1: Через команду open тест
-open -a Terminal
+#open -a Terminal
+# Способ 2: Через команду open с указанием пути
+ open -a Terminal /Users/username/Documents
+# Способ 3: Через команду open с указанием пути и параметром -n для нового окна
+# open -n -a Terminal /Users/username/Documents
+# Способ 4: Через команду open с указанием пути и параметром -a для приложения
+# open -a Terminal /Users/username/Documents
+# Способ 5: Через команду open с указанием пути и параметром -e для текстового редактора
